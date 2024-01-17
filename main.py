d2545ae7161cf6f19838d1f8f55230dd4adfacd0
Paarthavi <paarthavih2@gmail.com>
2024-01-17 16:29:53 +0530

    Day 46 - Added content to the table from the database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, \
-	QGridLayout, QPushButton, QMainWindow, QTableWidget
+	QGridLayout, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -21,13 +22,23 @@ class MainWindow(QMainWindow):
 		self.table = QTableWidget()
 		self.table.setColumnCount(4)
 		self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+		self.table.verticalHeader().setVisible(False)
 		self.setCentralWidget(self.table)
 
 	def load_data(self):
-		self.table
+		connection = sqlite3.connect("database.db")
+		result = connection.execute("SELECT * FROM students")
+		# print(list(result)) ----> prints out a list of tuples
+		self.table.setRowCount(0)
+		for row_number, row_data in enumerate(result):
+			self.table.insertRow(row_number)
+			for column_number, data in enumerate(row_data):
+				self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+		connection.close()
 
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
